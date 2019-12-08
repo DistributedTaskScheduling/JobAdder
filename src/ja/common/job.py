96c3a1dc9ddf147ce33a26933dbb2adf8ac2b435
Ilia Bozhinov <ammen99@gmail.com>
2019-12-08 11:35:15 +0100

    common: fix comments quotes

--- src/ja/common/job.py
+++ src/ja/common/job.py
@@ -1,14 +1,14 @@
-'''
+"""
 This module contains the definition of the Job class and related structures.
-'''
+"""
 from enum import Enum
 from ja.common.docker_context import DockerConstraints, IDockerContext
 
 
 class JobStatus(Enum):
-    '''
+    """
     Represents the status of a job.
-    '''
+    """
     NEW = 0
     QUEUED = 1
     RUNNING = 2
@@ -19,9 +19,9 @@ class JobStatus(Enum):
 
 
 class JobPriority(Enum):
-    '''
+    """
     Represents the base priority of a job.
-    '''
+    """
     LOW = 0
     MEDIUM = 1
     HIGH = 2
@@ -29,9 +29,13 @@ class JobPriority(Enum):
 
 
 class Job:
-    '''
+    """!
     Represents a job with all of its attributes.
-    '''
+    @param owner_id The unix user id of the user who owns this job.
+    @param priority The priority of the job.
+    @param ctx The docker context of the job.
+    @param constraints The docker constraints of the job.
+    """
     def __init__(self,
                  owner_id: int,
                  priority: JobPriority,
@@ -46,39 +50,41 @@ class Job:
 
     @property
     def uid(self) -> str:
-        '''
+        """!
         Get the job unique identifier used to create the job.
         If the job has not been queued yet, it has no UID.
-        '''
+        """
 
     @uid.setter
     def uid(self, value: str) -> None:
-        '''
+        """!
         Set the job UID.
         If the job has an UID already set, this results in a RuntimeError.
-        '''
+
+        @param value The new job UID.
+        """
 
     @property
     def owner_id(self) -> int:
-        '''
-        Get the id of the user who owns this job.
-        '''
+        """!
+        @return The id of the user who owns the job.
+        """
 
     @property
     def priority(self) -> JobPriority:
-        '''
-        Get the job's base priority.
-        '''
+        """!
+        @return The job priority.
+        """
 
     @property
     def status(self) -> JobStatus:
-        '''
-        Get the job status.
-        '''
+        """!
+        @return The current job status.
+        """
 
     @status.setter
     def status(self, new_status: JobStatus) -> None:
-        '''
+        """!
         Set the job status.
         The following transitions are allowed:
         NEW -> QUEUED
@@ -88,16 +94,18 @@ class Job:
 
         If the transition requested is not allowed, or if the job has not
         been assigned an UID, a RuntimeError will be raised.
-        '''
+
+        @param new_status The new status of the job.
+        """
 
     @property
     def docker_context(self) -> IDockerContext:
-        '''
-        Get the docker context of the job.
-        '''
+        """!
+        @return The docker context of the job.
+        """
 
     @property
     def docker_constraints(self) -> DockerConstraints:
-        '''
-        Get the docker constraints of the job.
-        '''
+        """!
+        @return The docker constraints of the job.
+        """
