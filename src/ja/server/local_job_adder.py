b403520e2d470e949b082318a996908d29be36c5
Ilia Bozhinov <ammen99@gmail.com>
2019-12-08 11:37:03 +0100

    use double quotes instead of single quotes in docstrings

--- src/ja/server/local_job_adder.py
+++ src/ja/server/local_job_adder.py
@@ -1,4 +1,4 @@
-'''
+"""
 LocalJobAdder is a single-class command line program.
 
 It verifies incoming user commands, checks that the user has sufficient
@@ -7,10 +7,10 @@ server socket.
 
 The commands are received on the standard input, and the server response
 will be printed on the standard output.
-'''
+"""
 
 
 class LocalJobAdder:
-    '''
+    """
     The main class of the program.
-    '''
+    """
