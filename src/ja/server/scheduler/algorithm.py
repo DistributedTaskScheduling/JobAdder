37d538b31f6c2ccf5a4cfcdb80e272b171251f28
Ilia Bozhinov <ammen99@gmail.com>
2020-01-06 15:48:13 +0200

    algorithm: add more details of the scheduling strategy

--- src/ja/server/scheduler/algorithm.py
+++ src/ja/server/scheduler/algorithm.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional, Tuple
 from ja.common.job import Job
 from ja.server.database.database import ServerDatabase
 from ja.server.database.types.work_machine import WorkMachine
@@ -7,8 +7,8 @@ from ja.server.database.types.work_machine import WorkMachine
 
 class CostFunction(ABC):
     """
-    The base class for any cost function that can be used with a scheduling
-    algorithm.
+    A CostFunction is a collection of functions which determine the order in
+    which jobs are scheduled.
     """
 
     @abstractmethod
@@ -23,6 +23,73 @@ class CostFunction(ABC):
         @return The calculated cost of the job.
         """
 
+    @abstractmethod
+    @property
+    def blocking_threshold(self) -> int:
+        """!
+        @return The minimum cost a job can have so that it can block further
+          jobs with smaller cost from being scheduled.
+        """
+
+    @abstractmethod
+    @property
+    def preempting_threshold(self) -> int:
+        """!
+        @return The minimum cost a job can have so that it can preempt already
+          running jobs. Should be at least @blocking_threshold.
+        """
+
+
+class JobDistributionPolicy(ABC):
+    """
+    JobDistributionPolicy is a collection of functions which determine how
+    jobs to be scheduled are to be distributed among work machines.
+    """
+
+    @abstractmethod
+    def assign_machine(self, job: Job, available_machines: List[WorkMachine]) -> WorkMachine:
+        """!
+        Find a machine to schedule @job on, without preempting any existing
+        jobs.
+
+        @param job The job to be scheduled.
+        @param available_machines A list of available work machines to
+          schedule @job on.
+        @return The chosen work machine (must be one of @available_machines),
+          or None if no suitable work machine was found.
+        """
+
+    @abstractmethod
+    def assign_blocked_machine(self,
+                               job: Job,
+                               distribution: ServerDatabase.JobDistribution,
+                               available_machines: List[WorkMachine]) -> WorkMachine:
+        """!
+        Find a machine to reserve for @job. Reserving a machine means no more
+        jobs with lower cost will be scheduled on the returned machine.
+
+        @param job The job to be scheduled.
+        @param distribution The existing distribution of jobs on work machines.
+        @param available_machines A list of available work machines to
+          assign as blocked for @job.
+        @return The chosen work machine (must be one of @available_machines),
+          or None if no suitable work machine was found.
+        """
+
+    @abstractmethod
+    def assign_machine_preemptive(self,
+                                  job: Job,
+                                  distribution: ServerDatabase.JobDistribution,
+                                  available_machines: List[WorkMachine]) -> Optional[Tuple[WorkMachine, List[Job]]]:
+        """!
+        Find a machine to schedule @job on, preemption of already running jobs is allowed.
+
+        @param job The job to be scheduled.
+        @param available_machines A list of available work machines to assign @job on.
+        @return If there is no suitable work machine where the job should be scheduled, return None.
+          Otherwise, return the assigned work machine and a list of jobs running on that machine to preempt.
+        """
+
 
 class SchedulingAlgorithm(ABC):
     """
@@ -32,8 +99,7 @@ class SchedulingAlgorithm(ABC):
     @abstractmethod
     def reschedule_jobs(self,
                         current_schedule: ServerDatabase.JobDistribution,
-                        available_machines: List[WorkMachine]) \
-            -> ServerDatabase.JobDistribution:
+                        available_machines: List[WorkMachine]) -> ServerDatabase.JobDistribution:
         """!
         Given the current list of jobs with their states and their assigned
         work machines, calculate the next states and assigned machines for
--- src/ja/server/scheduler/default_algorithm.py
+++ src/ja/server/scheduler/default_algorithm.py
@@ -1,15 +1,74 @@
-from typing import List
+from typing import List, Optional, Tuple
 from ja.common.job import Job
 from ja.server.database.database import ServerDatabase
 from ja.server.database.types.work_machine import WorkMachine
-from ja.server.scheduler.algorithm import SchedulingAlgorithm, CostFunction
+from ja.server.scheduler.algorithm import SchedulingAlgorithm, CostFunction, JobDistributionPolicy
 
 
 class DefaultCostFunction(CostFunction):
     """
-    The default cost function used in JobAdder.
+    The default cost function provided by JobAdder.
     """
+
     def calculate_cost(self, job: Job) -> int:
+        pass
+
+    @property
+    def blocking_threshold(self) -> int:
+        pass
+
+    @property
+    def preempting_threshold(self) -> int:
+        pass
+
+
+class DefaultJobDistributionPolicy(JobDistributionPolicy):
+    """
+    The default job distribution policy provided by JobAdder.
+    """
+
+    def __init__(self, cost_function: CostFunction):
+        """!
+        Create a DefaultJobDistributionPolicy.
+
+        @param cost_function The cost function to use.
+        """
+
+    def _assign_machine_cost(self, job: Job, machine: WorkMachine) -> Optional[int]:
+        """!
+        Calculate the cost of scheduling @job on @machine without preemption.
+        @return The cost, or None if @job can not be scheduled on @machine without preemption.
+        """
+
+    def _assign_blocked_machine_cost(self, job: Job, machine: WorkMachine, existing_jobs: List[Job]) -> Optional[int]:
+        """!
+        Calculate the cost of assigned @machine as blocked for @job.
+        @return The cost, of None if @job can not be scheduled on @machine.
+        """
+
+    def _assign_machine_preemptive_cost(self,
+                                        job: Job,
+                                        machine: WorkMachine,
+                                        existing_jobs: List[Job]) -> Optional[Tuple[int, List[Job]]]:
+        """!
+        Calculate the cost of assigning @machine for @job with preemption.
+        @return The cost of assigning @job to @machine and a list of jobs on @machine to preempt, or None if this is
+          not possible.
+        """
+
+    def assign_machine(self, job: Job, available_machines: List[WorkMachine]) -> WorkMachine:
+        pass
+
+    def assign_blocked_machine(self,
+                               job: Job,
+                               distribution: ServerDatabase.JobDistribution,
+                               available_machines: List[WorkMachine]) -> WorkMachine:
+        pass
+
+    def assign_machine_preemptive(self,
+                                  job: Job,
+                                  distribution: ServerDatabase.JobDistribution,
+                                  available_machines: List[WorkMachine]) -> Optional[Tuple[WorkMachine, List[Job]]]:
         pass
 
 
@@ -18,7 +77,9 @@ class DefaultSchedulingAlgorithm(SchedulingAlgorithm):
     The default scheduling algorithm used in JobAdder.
     """
 
-    def __init__(self, cost_function: CostFunction = DefaultCostFunction()):
+    def __init__(self,
+                 cost_function: CostFunction = DefaultCostFunction(),
+                 distribution_policy: JobDistributionPolicy = DefaultJobDistributionPolicy(DefaultCostFunction())):
         """!
         Initialize the scheduling algorithm.
 
