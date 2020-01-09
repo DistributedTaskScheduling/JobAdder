e2d52eef7e451699056fd3673d487a84a1e4054e
Ilia Bozhinov <ammen99@gmail.com>
2020-01-09 21:03:39 +0200

    add basic main class

--- src/ja/server/main.py
+++ src/ja/server/main.py
@@ -1,6 +1,21 @@
 class JobCenter:
     """
-    Main class for the central server. Contains singleton objects of the
+    Main class for the central server daemon. Contains singleton objects of the
     following classes: ServerCommandHandler, Database, Scheduler, Dispatcher,
     Notifier, HttpServer.
     """
+
+    def __init__(self) -> None:
+        """!
+        Initialize the JobAdder server daemon.
+        This includes:
+        1. Parsing the command line arguments and the configuration file.
+        2. Connecting to the configured database.
+        3. Initializing the scheduler and the dispatcher.
+        4. Starting the web server and the email notifier.
+        """
+
+    def run(self) -> None:
+        """
+        Run the main loop of the daemon and listen for incoming requests over the local socket.
+        """
