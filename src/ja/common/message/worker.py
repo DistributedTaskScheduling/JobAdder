dfc4fbfa9fd40db7b5ab8f3a99157818f916d2dd
nikolatzotchev <ntzotchev@gmail.com>
2020-01-10 10:56:13 +0100

    Adding commands for the worker.

--- src/ja/common/message/worker.py
+++ src/ja/common/message/worker.py
@@ -3,6 +3,7 @@ This module provides interfaces for Messages
 """
 from abc import ABC
 from ja.common.message.base import Command, Response
+from ja.worker.worker_client import Worker
 
 
 class WorkerCommand(Command, ABC):
@@ -11,12 +12,11 @@ class WorkerCommand(Command, ABC):
     and performed on the worker client.
     """
 
-    # TODO define target for worker client
-    def execute(self, target: object) -> "WorkerResponse":
+    def execute(self, worker_client: Worker) -> "WorkerResponse":
         """!
         Executes a WorkerCommand object on the worker and generates a
         WorkerResponse object to be sent back in return.
-        @param target:
+        @param worker_client: the Worker client that is going to be used to execute the command.
         @return: The WorkerResponse to be sent back.
         """
 
