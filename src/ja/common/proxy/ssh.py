50024bc0973a619ac469dd8c26123537c8eecc46
JohannesGaessler <johannesg@5d6.de>
2020-01-31 17:48:27 +0100

    Implemented SSHConfig + unit tests

--- src/ja/common/proxy/ssh.py
+++ src/ja/common/proxy/ssh.py
@@ -11,7 +11,6 @@ class SSHConnection:
     of the Remote objects. Message objects are read/written as YAML strings.
     Uses paramiko as the backend for establishing an ssh connection.
     """
-
     def __init__(self, ssh_config: "SSHConfig", remote: str):
         """!
         Creates a new SSHConnection object. Arguments for establishing the
@@ -41,27 +40,34 @@ class SSHConfig(Config):
     """
     Specifies all properties used for establishing a paramiko SSHConnection.
     """
-
-    def __init__(self, hostname: str, username: str, password: str = None,
-                 key_filename: str = None, passphrase: str = None):
+    def __init__(self, hostname: str, username: str = None, password: str = None,
+                 key_path: str = None, passphrase: str = None):
         """!
         For the SSHConnection some sort of credentials must be provided: either
         a password or a private key (which might need a passphrase to access).
         Keys placed in ~/.ssh are automatically used if possible.
         @param hostname: The name of the host to connect to, mandatory.
-        @param username: The name of the user to use for login, mandatory.
+        @param username: The name of the user to use for login, optional.
         @param password: The password to use for login, optional.
-        @param key_filename: The key pair to use for login, optional.
+        @param key_path: The key pair to use for login, optional.
         @param passphrase: The passphrase to use for decrypting private keys,
         optional.
         """
         self._hostname = hostname
         self._username = username
         self._password = password
-        self._key_filename = key_filename
+        self._key_path = key_path
         self._passphrase = passphrase
-        if self._key_filename is None:
-            self._key_filename = "~/.ssh/id_rsa"
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, SSHConfig):
+            return self.hostname == other.hostname \
+                and self.username == other.username \
+                and self.password == other.password \
+                and self.key_path == other.key_path \
+                and self.passphrase == other.passphrase
+        else:
+            return False
 
     @property
     def hostname(self) -> str:
@@ -85,11 +91,11 @@ class SSHConfig(Config):
         return self._password
 
     @property
-    def key_filename(self) -> str:
+    def key_path(self) -> str:
         """!
         @return: The key pair to use for login.
         """
-        return self._key_filename
+        return self._key_path
 
     @property
     def passphrase(self) -> str:
@@ -98,32 +104,22 @@ class SSHConfig(Config):
         """
         return self._passphrase
 
-    def __eq__(self, o: object) -> bool:
-        if isinstance(o, SSHConfig):
-            return self._hostname == o._hostname \
-                and self._username == o._username \
-                and self._key_filename == o._key_filename \
-                and self._password == o._password \
-                and self._passphrase == o._passphrase
-        else:
-            return False
-
     def to_dict(self) -> Dict[str, object]:
-        _dict: Dict[str, object] = dict()
-        _dict["hostname"] = self._hostname
-        _dict["username"] = self._username
-        _dict["password"] = self._password
-        _dict["key_filename"] = self._key_filename
-        _dict["passphrase"] = self._passphrase
-        return _dict
+        return_dict: Dict[str, object] = dict()
+        return_dict["hostname"] = self.hostname
+        return_dict["username"] = self.username
+        return_dict["password"] = self.password
+        return_dict["key_path"] = self.key_path
+        return_dict["passphrase"] = self.passphrase
+        return return_dict
 
     @classmethod
     def from_dict(cls, property_dict: Dict[str, object]) -> "SSHConfig":
-        _hostname = cls._get_str_from_dict(property_dict=property_dict, key="hostname")
-        _username = cls._get_str_from_dict(property_dict=property_dict, key="username")
-        _password = cls._get_str_from_dict(property_dict=property_dict, key="password", mandatory=False)
-        _key_filename = cls._get_str_from_dict(property_dict=property_dict, key="key_filename", mandatory=False)
-        _passphrase = cls._get_str_from_dict(property_dict=property_dict, key="passphrase", mandatory=False)
+        hostname = cls._get_str_from_dict(property_dict=property_dict, key="hostname")
+        username = cls._get_str_from_dict(property_dict=property_dict, key="username", mandatory=False)
+        password = cls._get_str_from_dict(property_dict=property_dict, key="password", mandatory=False)
+        key_path = cls._get_str_from_dict(property_dict=property_dict, key="key_path", mandatory=False)
+        passphrase = cls._get_str_from_dict(property_dict=property_dict, key="passphrase", mandatory=False)
         cls._assert_all_properties_used(property_dict)
-        return SSHConfig(hostname=_hostname, username=_username, password=_password, key_filename=_key_filename,
-                         passphrase=_passphrase)
+        return SSHConfig(
+            hostname=hostname, username=username, password=password, key_path=key_path, passphrase=passphrase)
