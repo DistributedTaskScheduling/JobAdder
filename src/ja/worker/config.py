0f836b07908b050fa033c25046afc33cd317af74
jooe1 <jamilooojamil@hotmail.com>
2020-02-04 21:20:02 +0100

    added the ip and delete the comment

--- src/ja/worker/config.py
+++ src/ja/worker/config.py
@@ -9,14 +9,14 @@ class WorkerConfig(Config):
     Config for the worker client.
     """
 
-    def __init__(self, uid: str, ssh_config: SSHConfig, resource_allocation: ResourceAllocation):
+    def __init__(self, uid: str, ssh_config: SSHConfig, resource_allocation: ResourceAllocation, ip: str):
         """!
-        reads the config file from the disk
         creates resource allocation instance
         """
         self._uid = uid
         self._ssh_config = ssh_config
         self._resource_allocation = resource_allocation
+        self._ip = ip
 
     @property
     def uid(self) -> str:
@@ -24,6 +24,13 @@ class WorkerConfig(Config):
         @return: The desired UID to register with on the server.
         """
         return str(self._uid)
+
+    @property
+    def ip(self) -> str:
+        """!
+        @return: The IP of the central server .
+        """
+        return str(self._ip)
 
     @property
     def ssh_config(self) -> SSHConfig:
@@ -44,6 +51,7 @@ class WorkerConfig(Config):
         d["ssh_config"] = self._ssh_config.to_dict()
         d["resource_allocation"] = self._resource_allocation.to_dict()
         d["uid"] = self._uid
+        d["ip"] = self._ip
         return d
 
     @classmethod
@@ -53,4 +61,5 @@ class WorkerConfig(Config):
         resource_allocation = ResourceAllocation.from_dict(
             cls._get_dict_from_dict(property_dict=property_dict, key="resource_allocation", mandatory=False))
         uid = str(property_dict["uid"])
-        return WorkerConfig(uid, ssh_config, resource_allocation)
+        ip = str(property_dict["ip"])
+        return WorkerConfig(uid, ssh_config, resource_allocation, ip)
