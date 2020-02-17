0e325667392edac6bc37390d447f58b10f870906
jooe1 <jamilooojamil@hotmail.com>
2020-02-17 13:31:36 +0100

    added AbstractSerializableTest test

--- src/test/worker/config.py
+++ src/test/worker/config.py
@@ -1,6 +1,7 @@
 from ja.common.proxy.ssh import SSHConfig
 from ja.common.work_machine import ResourceAllocation
 from ja.worker.config import WorkerConfig
+from test.serializable.base import AbstractSerializableTest
 from unittest import TestCase
 from typing import Dict
 
@@ -37,3 +38,39 @@ class WorkMachineConfigTest(TestCase):
         # self.assertDictEqual(self._wmc, d)
         # wc = WorkerConfig.from_dict(d)
         # self.assertDictEqual(wc, d)
+
+class WorkerMachineDictTest(AbstractSerializableTest)
+    """
+    using the AbstractSerializableTest tests
+    """
+
+    def setUp(self) -> None:
+        self._optional_properties = []
+        self._object  = WorkerConfig(ssh_config=SSHConfig(hostname="127.0.1.1", username="someuser"),
+                                                       resource_allocation=ResourceAllocation(1, 2, 3), uid="3")
+        self._object_dict = {
+            "uid": "3",
+            "ssh_config": {
+                "hostname": "127.0.1.1",
+                "username": "someuser"
+            }
+            "resource_allocation": {
+                "cpu_threads": 1,
+                "memory": 2,
+                "swap": 3
+            }
+        }    
+
+        self._other_object_dict  = {
+            "uid": "4",
+            "ssh_config": {
+                "hostname": "127.0.1.2",
+                "username": "someuser"
+            }
+            "resource_allocation": {
+                "cpu_threads": 1,
+                "memory": 2,
+                "swap": 3
+            }
+        }    
+
