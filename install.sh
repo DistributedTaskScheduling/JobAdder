78b40e13e96fecd6d2f3b460666e5cc65ffb4570
Ilia Bozhinov <ammen99@gmail.com>
2020-03-08 20:06:02 +0100

    fixup! server: add systemd service

--- install.sh
+++ install.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root."
+   exit 1
+fi
+
 PREFIX=/usr
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Install JobAdder source code + binaries
 # FIXME: how to make setup.py ignore test folder??
-mv ./src/test/__init__.py ./src/test/init
-./src/setup.py install --prefix=$PREFIX
-mv ./src/test/init ./src/test/__init__.py
+cd $SCRIPTPATH/src/
+mv ./test/__init__.py ./test/init
+./setup.py install --prefix=$PREFIX
+mv ./test/init ./test/__init__.py
+cd $SCRIPTPATH
 
 # TODO: when remote is ready, set SUID
 
 # Install Systemd unit files
-cp src/bin/JobAdderServer.service /etc/systemd/system/
+SERVER_SRV_FILE=/etc/systemd/system/JobAdderServer.service
+
+cp src/bin/JobAdderServer.service $SERVER_SRV_FILE
+chmod 644 $SERVER_SRV_FILE
+
+# Install config file
+CONFIG_FILE=/etc/jobadder/server.conf
+
+if [ ! -f $CONFIG_FILE ]; then
+    mkdir -p /etc/jobadder/
+    cp ./config-files/server.conf $CONFIG_FILE
+    chmod 644 $CONFIG_FILE
+fi
